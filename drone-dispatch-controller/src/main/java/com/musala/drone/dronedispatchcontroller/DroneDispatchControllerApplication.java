065e8665e0f7746dbcb986221f053681e45e214b
okpakomarvis <okpakomarvis@gmail.com>
2023-08-17 10:59:23 +0100

    Added spring boot with spring starter dependencies

---
+++ drone-dispatch-controller/src/main/java/com/musala/drone/dronedispatchcontroller/DroneDispatchControllerApplication.java
@@ -0,0 +1,13 @@
+package com.musala.drone.dronedispatchcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDispatchControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDispatchControllerApplication.class, args);
+	}
+
+}
---
+++ drone-dispatch-controller/src/test/java/com/musala/drone/dronedispatchcontroller/DroneDispatchControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.drone.dronedispatchcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDispatchControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
